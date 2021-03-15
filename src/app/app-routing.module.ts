2593dca53a003d99c78544bb34aaa0b911635624
Bob Parsons <bparsons0904@gmail.com>
2021-03-14 22:55:03 -0500

    Base header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { LoginComponent } from './components/pages/authentication/login/login.component';
 import { RegisterComponent } from './components/pages/authentication/register/register.component';
 import { ProfileComponent } from './components/pages/authentication/profile/profile.component';
+// General Components
+import { AboutComponent } from './components/pages/general/about/about.component';
+import { DemoComponent } from './components/pages/general/demo/demo.component';
+import { PricingComponent } from './components/pages/general/pricing/pricing.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,6 +17,9 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'demo', component: DemoComponent },
+  { path: 'pricing', component: PricingComponent },
   { path: '**', redirectTo: '' },
 ];
 
