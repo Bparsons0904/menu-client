da9dc2f20fc8ac45aad7125c6d0917f21288c47a
Bob Parsons <bparsons0904@gmail.com>
2021-03-15 20:43:35 -0500

    Adjusted drop down menu

--- src/app/components/navigation/navbar/navbar.component.ts
+++ src/app/components/navigation/navbar/navbar.component.ts
@@ -7,11 +7,31 @@ import { AuthService } from '../../../services/auth.services';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  public isAuthenticated: boolean = false;
+
   constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authService.isAuthenticated().subscribe((user) => {
+      this.isAuthenticated = user;
+    });
+  }
 
   public logout(): void {
     this.authService.logout();
+  }
+
+  public toggleCheckbox(event): void {
+    const elements: HTMLCollection = document.getElementsByClassName('active');
+    Array.from(elements).forEach((element) => {
+      element.classList.remove('active');
+    });
+    console.log(event.target);
+    const checkbox: HTMLInputElement = <HTMLInputElement>(
+      document.getElementById('toggler')
+    );
+    checkbox.checked = !checkbox.checked;
+    const target: HTMLElement = document.getElementById(event.target.id);
+    target.classList.add('active');
   }
 }
